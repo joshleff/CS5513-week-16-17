84bb2b50aa40d3364b7f372e4e8fb7df78af18cb
joshleff <joshleffler@tuta.io>
2023-12-15 11:10:52 -0800

    added display of data from REST API

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,13 +1,34 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+    IonContent,
+    IonHeader, IonImg,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonListHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import {useEffect, useState} from "react";
 
 const Tab1: React.FC = () => {
+
+    const [dataset, setDataset] = useState<any[]>([]);
+    // URL for WP JSON REST endpoint
+    const dataURL = "https://dev-cs5513-week-11.pantheonsite.io/wp-json/twentytwentyfour-child/v1/products";
+    // useEffect() to get JSON data and populate dataset state variable
+    useEffect(() => {
+        fetch(dataURL) // fetch() to load raw json string
+            .then(response => response.json()) // json() to convert raw string to json object
+            .then(data => setDataset(data)) // react state set function to populate data state var
+    },[])
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>PRODUCTS</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,8 +37,24 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
+          <IonList id="thing-list" class="">
+              <IonListHeader class="">ON SALE NOW!</IonListHeader>
+              {dataset.map((item, index) => (
+                  <IonItem lines="inset" key={index}>
+                      <IonLabel>
+                          <IonImg
+                              src={item.image_url}
+                              alt={item.product_name}
+                              class="ion-float-start ion-margin"
+                          ></IonImg>
+
+                          <div class="ion-margin"><h2>{item.product_name}</h2></div>
+                          <div className="ion-margin"><p>{item.price}</p></div>
+                          <div className="ion-margin"><p>{item.description}</p></div>
+                      </IonLabel>
+                  </IonItem>
+              ))}
+          </IonList>      </IonContent>
     </IonPage>
   );
 };
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,13 +1,33 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+    IonContent,
+    IonHeader,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonListHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import {useEffect, useState} from "react";
 
 const Tab3: React.FC = () => {
+    const [dataset, setDataset] = useState<any[]>([]);
+    // URL for WP JSON REST endpoint
+    const dataURL = "https://dev-cs5513-week-11.pantheonsite.io/wp-json/twentytwentyfour-child/v1/customers";
+    // useEffect() to get JSON data and populate dataset state variable
+    useEffect(() => {
+        fetch(dataURL) // fetch() to load raw json string
+            .then(response => response.json()) // json() to convert raw string to json object
+            .then(data => setDataset(data)) // react state set function to populate data state var
+    },[])
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>CUSTOMERS</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +36,18 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+          <IonList id="thing-list">
+              <IonListHeader>WHAT GREAT CUSTOMERS</IonListHeader>
+              {dataset.map((item, index) => (
+                  <IonItem lines="inset" key={index}>
+                      <IonLabel>
+                          <div className="ion-margin"><h4>{item.name}</h4></div>
+                          <div className="ion-margin"><p>{item.phone_number}</p></div>
+                          <div className="ion-margin">   <p>{item.email}</p></div>
+                      </IonLabel>
+                  </IonItem>
+              ))}
+          </IonList>
       </IonContent>
     </IonPage>
   );
